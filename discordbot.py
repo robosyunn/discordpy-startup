b6d77bfa5a5a1dde3d1b0f13b290b2f3f851820d
たにしゅん <robosyunn0319@gmail.com>
2019-10-25 09:48:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,24 +18,6 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
-    
-@client.event
-async def on_message(message):
-    # メンバーのリストを取得して表示
-    if message.content == '/members':
-        print(message.guild.members)
-    # 役職のリストを取得して表示
-    if message.content == '/roles':
-        print(message.guild.roles)
-    # テキストチャンネルのリストを取得して表示
-    if message.content == '/text_channels':
-        print(message.guild.text_channels)
-    # ボイスチャンネルのリストを取得して表示
-    if message.content == '/voice_channels':
-        print(message.guild.voice_channels)
-    # カテゴリチャンネルのリストを取得して表示
-    if message.content == '/category_channels':
-        print(message.guild.categories)    
 
         
 
