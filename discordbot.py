2a2905cbb714d00cf212cca7f3f697c28d3df526
robosyunn <robosyunn0319@gmail.com>
2020-04-13 13:41:15 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -7,29 +7,31 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
 
-# @bot.event
-# async def on_command_error(ctx, error):
-#     await ctx.send(str(error))
+
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
 
 
-# @bot.command()
-# async def ping(ctx):
-#     await ctx.send('pong')
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
-# @bot.command()
-# async def neko(ctx):
-#     await ctx.send('にゃーん')
+@client.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')
 
 
-# @bot.command()
-# async def aho(ctx):
-#     await ctx.send('にょーん')
+@client.command()
+async def aho(ctx):
+    await ctx.send('にょーん')
 
 
-# @bot.command()
-# async def 月曜日(ctx):
-#     await ctx.send('数学A　体育　政治経済')
+@client.command()
+async def 月曜日(ctx):
+    await ctx.send('数学A　体育　政治経済')
+
 
 @client.event
 async def on_message(message):
@@ -39,4 +41,4 @@ async def on_message(message):
         await message.channel.send('にゃーん')
 
 
-bot.run(token)
+client.run(token)
