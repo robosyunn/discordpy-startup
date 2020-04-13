1a27f7cfdc8efb6517cd93a02f4f2a07494d3db9
robosyunn <robosyunn0319@gmail.com>
2020-04-13 13:35:11 +0900

    てｓｔ

--- discordbot.py
+++ discordbot.py
@@ -2,33 +2,41 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+# bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# @bot.event
+# async def on_command_error(ctx, error):
+#     await ctx.send(str(error))
 
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send('pong')
 
 
-@bot.command()
-async def aho(ctx):
-    await ctx.send('にょーん')
+# @bot.command()
+# async def neko(ctx):
+#     await ctx.send('にゃーん')
 
 
-@bot.command()
-async def 月曜日(ctx):
-    await ctx.send('数学A　体育　政治経済')
+# @bot.command()
+# async def aho(ctx):
+#     await ctx.send('にょーん')
+
+
+# @bot.command()
+# async def 月曜日(ctx):
+#     await ctx.send('数学A　体育　政治経済')
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
 bot.run(token)
