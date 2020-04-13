733b745569545d14833f430be0100493ab607b11
robosyunn <robosyunn0319@gmail.com>
2020-04-13 13:45:32 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -2,43 +2,43 @@ from discord.ext import commands
 import os
 import traceback
 
-# bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-client = discord.Client()
+# client = discord.Client()
 
 
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-@client.command()
+@bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
 
 
-@client.command()
+@bot.command()
 async def aho(ctx):
     await ctx.send('にょーん')
 
 
-@client.command()
+@bot.command()
 async def 月曜日(ctx):
     await ctx.send('数学A　体育　政治経済')
 
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+# @client.event
+# async def on_message(message):
+#     if message.author.bot:
+#         return
+#     if message.content == '/neko':
+#         await message.channel.send('にゃーん')
 
 
-client.run(token)
+bot.run(token)
